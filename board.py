3dd0d8114460285db81a70fa159444469d7cab5c
BERSEK <zamurijevgenyij@gamil.com>
2025-01-27 00:07:10 +0300

    optimization update

--- board.py
+++ board.py
@@ -24,28 +24,12 @@ class Board(pygame.sprite.Sprite):
         self.surface = pygame.Surface((width * self.cell_size, height * self.cell_size))
         self.rect = self.surface.get_rect(topleft=(self.left, self.top))
 
-    def set_view(self, left, top, cell_size, colliders, vertical_borders):  # настройка внешнего вида
+    def set_view(self, left, top, cell_size):  # настройка внешнего вида
         self.left = left
         self.top = top
         self.cell_size = cell_size
-        self.create_borders(colliders, vertical_borders)
         self.surface = pygame.Surface((self.width * self.cell_size, self.height * self.cell_size))
         self.rect = self.surface.get_rect(topleft=(self.left, self.top))
-
-    def create_borders(self, colliders, vertical_borders):
-        # делаем стенки нашего поля, левое - правое - нижнее
-        x1, y1 = self.rect.x, self.rect.y
-        x2, y2 = x1 + (self.width * self.cell_size), y1 + (self.height * self.cell_size)
-        # создание боковых границ
-        left_rect = pygame.Rect(x1 - 50, y1 - 100, 50, self.cell_size * self.height + 100)
-        right_rect = pygame.Rect(x2, y1 - 100, 50, self.cell_size * self.height + 100)
-        # создадим rect для нижней границы
-        horizontal_rect = pygame.Rect(x1, y2, self.width * self.cell_size, 40)
-        colliders.append(left_rect)
-        colliders.append(right_rect)
-        colliders.append(horizontal_rect)
-        vertical_borders.append(left_rect)
-        vertical_borders.append(right_rect)
 
     def render(self, screen):
         # отрисовка нашего игрового поля
@@ -68,18 +52,6 @@ class Board(pygame.sprite.Sprite):
                     pygame.draw.line(screen, COLOR[color_index][3], (x, y + 1), (x + w, y + 1), 2)
                     pygame.draw.line(screen, COLOR[color_index][3], (x + 1, y), (x + 1, y + h), 2)
 
-    def update(self, colliders, vertical_borders):
-        # функция для обновления списка коллайдеров
-        self.create_borders(colliders, vertical_borders)
-        for i, line in enumerate(self.board):
-            for j, elem in enumerate(line):
-                # если элемент не пустой создаём коллайдер
-                if bool(elem):
-                    x = self.cell_size * j + self.rect.x
-                    y = self.cell_size * i + self.rect.y
-                    rect = pygame.Rect(x, y, self.cell_size, self.cell_size)
-                    colliders.append(rect)
-
     def get_cell(self, pos):
         # узнаем клетку поля по координатам окна
         x, y = pos
@@ -89,7 +61,7 @@ class Board(pygame.sprite.Sprite):
         y //= self.cell_size
         if (x >= 0 and y >= 0) and (
                 x < self.width and y < self.height):
-            return x, y
+            return [x, y]
         return None
 
     def create_block(self, cell, color_index):
@@ -119,7 +91,7 @@ class Board(pygame.sprite.Sprite):
             self.broken_block(x, y, particles_group)
 
     def broken_block(self, x, y, particles_group):
-        amount_particles = 7
+        amount_particles = 5
         left, top = self.rect.x, self.rect.y
         xx = left + self.cell_size * x
         yy = top + self.cell_size * y
